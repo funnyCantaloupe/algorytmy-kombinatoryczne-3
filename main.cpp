4d891627a926f9b4fbf141a8d75a2fa9992648d7
Aleksandra Karwat <62639078+funnyCantaloupe@users.noreply.github.com>
2023-01-04 21:22:47 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,19 +6,26 @@
 
 using namespace std;
 
+vector<pair<char, int>> nucleotide_cred1;
+vector<pair<char, int>> nucleotide_cred2;
+vector<pair<char, int>> nucleotide_cred3;
+vector<pair<char, int>> nucleotide_cred4;
+vector<pair<char, int>> nucleotide_cred5;
+
+void pair_up(char letter, int cred, int sequence_id) {
+    if (sequence_id == 1)
+        nucleotide_cred1.emplace_back(make_pair(letter, cred));
+    else if (sequence_id == 2)
+        nucleotide_cred2.emplace_back(make_pair(letter, cred));
+    else if (sequence_id == 3)
+        nucleotide_cred3.emplace_back(make_pair(letter, cred));
+    else if (sequence_id == 4)
+        nucleotide_cred4.emplace_back(make_pair(letter, cred));
+    else if (sequence_id == 5)
+        nucleotide_cred5.emplace_back(make_pair(letter, cred));
+}
+
 int main() {
-
-    vector<pair<char, int>> nucleotide_cred1;
-    vector<pair<char, int>> nucleotide_cred2;
-    vector<pair<char, int>> nucleotide_cred3;
-    vector<pair<char, int>> nucleotide_cred4;
-    vector<pair<char, int>> nucleotide_cred5;
-
-    nucleotide_cred1.emplace_back(make_pair('-', 0));
-    nucleotide_cred2.emplace_back(make_pair('-', 0));
-    nucleotide_cred3.emplace_back(make_pair('-', 0));
-    nucleotide_cred4.emplace_back(make_pair('-', 0));
-    nucleotide_cred5.emplace_back(make_pair('-', 0));
 
     // otwieranie plikow
 
@@ -28,6 +35,7 @@ int main() {
 
     if (!fastafile) {
         cout << "Blad otwierania pliku typu .fasta." << endl;
+        exit(1);
     }
 
     ifstream qualfile;
@@ -35,6 +43,7 @@ int main() {
 
     if (!qualfile) {
         cout << "Blad otwierania pliku typu .qual." << endl;
+        exit(1);
     }
 
     ifstream qual_pomoc;
@@ -43,6 +52,7 @@ int main() {
 
     if (!qual_pomoc) {
         cout << "Blad otwierania pomocnieczego pliku typu .qual." << endl;
+        exit(1);
     }
 
     // wczytywanie danych z pliku
@@ -50,15 +60,22 @@ int main() {
     vector<string> lines;
     string line;
     string line_pomoc;
-    char i = '\n'; // tez pomoc line
-    int lines_nr = 0;
+    int lines_nr;
+    int linie_w_pliku = 0;
     int spaces;
     int licznik; // licznik nukleotydow
+    int przelacznik = 0; // zeby nie robic 2x emplace_back dla licznik == spaces
+    bool dolaczono = false;
+    int spacje_count;
+    bool last_in_line = false;
 
     while (!qual_pomoc.eof()) {
         getline(qual_pomoc, line);
         lines.push_back(line);
+        linie_w_pliku++;
     }
+    linie_w_pliku--;
+    qual_pomoc.close();
 
     string fasta;
     string qual;
@@ -68,7 +85,8 @@ int main() {
     int cred;
     int sequence_id = 1;
 
-    while (!fastafile.eof()) {
+
+    while (sequence_id < 6) {
         licznik = 0;
         while (!fastafile.eof()) {
             getline(fastafile, fasta);
@@ -94,8 +112,9 @@ int main() {
             }
         }
 
+        cout << "linie w pliku: " << linie_w_pliku << endl;
+
         while (letter != '>' && letter != EOF) {
-            if (letter != '>') {
                 letter = fastafile.get();
                 if (letter == '\n') {
                     letter = fastafile.get();
@@ -107,7 +126,14 @@ int main() {
                         licznik++;
                         cout << "credibility: " << credibility << " letter: " << letter << endl;
                         cred = stoi(credibility);
-                    } else {
+                    }
+                    else if (lines_nr == linie_w_pliku && licznik == spaces) {
+                        getline(qualfile, credibility, ' ');
+                        cred = stoi(credibility);
+                        pair_up(letter, cred, sequence_id);
+                        break; // nie usuwac!
+                    }
+                    else {
                         lines_nr++;
                         getline(qualfile, credibility, '\n');
                         cout << "credibility: " << credibility << " letter: " << letter << endl;
@@ -129,45 +155,58 @@ int main() {
                         }
                         if (line_pomoc[0] == '>') {
                             cout << "line_pomoc break: " << line_pomoc << " line_nr: " << lines_nr << endl;
+                            cred = stoi(credibility);
+                            pair_up(letter, cred, sequence_id);
                             break;
                         }
                         else {
                             cout << "line_pomoc: " << line_pomoc << " line_nr: " << lines_nr << endl;
                             cred = stoi(credibility);
+                            pair_up(letter, cred, sequence_id);
+                            dolaczono = true;
                         }
-                        if (sequence_id == 1)
-                            nucleotide_cred1.emplace_back(make_pair(letter, cred));
-                        else if (sequence_id == 2)
-                            nucleotide_cred2.emplace_back(make_pair(letter, cred));
-                        else if (sequence_id == 3)
-                            nucleotide_cred3.emplace_back(make_pair(letter, cred));
-                        else if (sequence_id == 4)
-                            nucleotide_cred4.emplace_back(make_pair(letter, cred));
-                        else if (sequence_id == 5)
-                            nucleotide_cred5.emplace_back(make_pair(letter, cred));
-
-                        cred = stoi(line_pomoc);
                     }
-                        if (sequence_id == 1)
-                            nucleotide_cred1.emplace_back(make_pair(letter, cred));
-                        else if (sequence_id == 2)
-                            nucleotide_cred2.emplace_back(make_pair(letter, cred));
-                        else if (sequence_id == 3)
-                            nucleotide_cred3.emplace_back(make_pair(letter, cred));
-                        else if (sequence_id == 4)
-                            nucleotide_cred4.emplace_back(make_pair(letter, cred));
-                        else if (sequence_id == 5)
-                            nucleotide_cred5.emplace_back(make_pair(letter, cred));
+                    if (!dolaczono) {
+                        pair_up(letter, cred, sequence_id);
+                    }
+                    else {
+                        dolaczono = false;
                     }
                 }
             }
             sequence_id++;
+            cout << "test" << endl;
         }
 
-        cout << "sekwencja 1: ";
+    cout << "czy to dziala?" << endl;
+
+        cout << "sekwencja 1: " << endl;
         for (auto &n: nucleotide_cred1) {
             cout << n.first << ' ' << n.second << endl;
         }
+
+        cout << "sekwencja 2: " << endl;
+        for (auto &n: nucleotide_cred2) {
+            cout << n.first << ' ' << n.second << endl;
+        }
+
+        cout << "sekwencja 3: " << endl;
+        for (auto &n: nucleotide_cred3) {
+            cout << n.first << ' ' << n.second << endl;
+        }
+
+        cout << "sekwencja 4: " << endl;
+        for (auto &n: nucleotide_cred4) {
+            cout << n.first << ' ' << n.second << endl;
+        }
+
+        cout << "sekwencja 5: " << endl;
+        for (auto &n: nucleotide_cred5) {
+            cout << n.first << ' ' << n.second << endl;
+        }
+
+        fastafile.close();
+        qualfile.close();
 
         return 0;
     }
